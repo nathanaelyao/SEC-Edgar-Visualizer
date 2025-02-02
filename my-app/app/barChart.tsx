ed984ff24d61cf3bdb0b56db9472806568f63e6e
Nathanael Yao <18ny13@queensu.ca>
2025-02-02 18:20:08 -0500

    update readme

--- my-app/app/barChart.tsx
+++ my-app/app/barChart.tsx
@@ -64,7 +64,7 @@ const BarGraph = ({data}) => {
         </Svg>
         {selectedValue && (
         <View style={styles.valueDisplay}>
-            <Text style={styles.valueText}>Revenue: {selectedValue}$</Text>
+            <Text style={styles.valueText}>{selectedValue}$</Text>
         </View>
         )}
         <View style={styles.labels}>
